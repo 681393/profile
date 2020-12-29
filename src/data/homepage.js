905cc3588c577f3e4a5d248e48b00c17c78e5643
Himanshu Jain <jain.himanshu1990@gmail.com>
2020-12-29 17:03:04 +0530

    Updates

--- src/data/homepage.js
+++
@@ -1,7 +0,0 @@
-export const homepage = {
-  name: "Himanshu Jain",
-  role: "Java Developer",
-  email: "jain.himanshu1990@gmail.com",
-  phone: "+91 9711681255",
-  location: "Noida, India",
-};
--- src/data/menuItems.js
+++
@@ -1,8 +0,0 @@
-export const menuItems = [
-  { item: "Home", link: "#home", active: "active" },
-  { item: "About Me", link: "#aboutme-section" },
-  { item: "Skills", link: "#skill-section" },
-  { item: "Experience", link: "#experience-section" },
-  { item: "Certifications", link: "#certification-section" },
-  { item: "Education", link: "#education-section" },
-];
